1d409c8807efda6730e80d2c3066d2731092fe31
anaism78 <anais.martin@cri-paris.org>
2016-12-16 15:42:55 +0100

    up

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -1,4 +1,3 @@
-from string import ascii_lowercase
-word = ascii_lowercase[::-1]
-print(word[0:5] + 'u' + word[6:11] + 'O' + word[12:17] + 'I' + word[18:21] + 'E' + word[22:25] + 'A')
+#!/usr/bin/python
+print("zyxwvUtsrqpOnmlkjIhgfEdcbA")
 
